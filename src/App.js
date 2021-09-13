8c14f60ccf37be50fa7f4792de4acd53163fe6e8
GuptaEesh <90090601+GuptaEesh@users.noreply.github.com>
2021-09-13 19:58:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "./styles.css";
+const InvestmentWays = {
+  CryptoCurrencies: [
+    {
+      name: "Dogecoin",
+      value: "Ð"
+    },
+    {
+      name: "Ethereum",
+      value: "Ξ"
+    },
+    {
+      name: "Litecoin",
+      value: "Ł"
+    }
+  ],
+  "ResidentialPrice(/900sqft)": [
+    {
+      name: "Haryana",
+      value: "12 Lakhs"
+    },
+    {
+      name: "Punjab",
+      value: "40 Lakhs"
+    },
+    {
+      name: "Karnataka",
+      value: "33 Lakhs"
+    }
+  ],
+  "SIP Types": [
+    {
+      name: "Regular",
+      value: "Periodic investments needed"
+    },
+    {
+      name: "Top-Up",
+      value: "Can increase periodic investments"
+    },
+    {
+      name: "Flexible",
+      value: "No need of periodicity in investments"
+    }
+  ]
+};
+var select = Object.keys(InvestmentWays);
+export default function App() {
+  var [investOpt, investDetail] = useState("CryptoCurrencies");
+  function clickHandler(investOpt) {
+    investDetail(investOpt);
+  }
+  return (
+    <div className="App">
+      <div className="prime">
+        <br></br>
+        <h1>Investment Options </h1>
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <br></br>
+        {select.map((investOpt) => {
+          return (
+            <button key={investOpt} onClick={() => clickHandler(investOpt)}>
+              {investOpt}
+            </button>
+          );
+        })}
+      </div>
+      <ul>
+        {InvestmentWays[investOpt].map((details) => (
+          <li>
+            <div>
+              Name: {details.name}
+              {" 🎁 "}
+              Value: {details.value}
+            </div>
+          </li>
+        ))}
+      </ul>
+      <div className="finalBtn">
+        <button>
+          <a href="/">Source Code</a>
+        </button>
+      </div>
+    </div>
+  );
+}
